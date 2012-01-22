aec06ef0c7a15b3f19bcf352d4880c3e3589717a
Michael Nutt <michael@nuttnet.net>
2012-01-22 00:14:51 -0500

    more logging on control

--- control.js
+++ control.js
@@ -1,8 +1,20 @@
+var child_process = require('child_process');
 var app = require('express').createServer();
 
 app.get("/", function(req, res) {
   console.log("SHUTDOWN");
-//  require('child_process').exec("sudo killall apcupsd && sudo apcupsd -f /etc/apcupsd/apcupsd.conf -o", function(err, stdout, stderr) {});
+  require('child_process').exec("sudo killall apcupsd", function(err, stdout, stderr) {
+    console.log(stderr);
+    console.log(stdout);
+    child_process.exec("sudo apcupsd -f /etc/apcupsd/apcupsd.conf -o", function(err, stdout, stderr) {
+      console.log(stderr);
+      console.log(stdout);
+      child_process.exec("sleep 5; sudo apcupsd -f /etc/apcupsd/apcupsd.conf", function(err, stdout, stderr) {
+        console.log(stderr);
+        console.log(stdout);
+      });
+    });
+  });
   res.writeHead(200, {});
   res.end("ok");
 });
